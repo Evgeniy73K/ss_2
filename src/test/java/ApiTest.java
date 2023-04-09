b44d099b62a71c91e0fc4c8790177d1a0c7c428a
evg <evg73ul@mail.ru>
2023-04-09 18:13:24 +0400

    refactor

--- src/test/java/ApiTest.java
+++ src/test/java/ApiTest.java
@@ -6,54 +6,44 @@ import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.everyItem;
-import static org.hamcrest.Matchers.hasItem;
-import static org.hamcrest.Matchers.lessThan;
 import static org.hamcrest.Matchers.notNullValue;
+import static org.testng.Assert.assertTrue;
 
 public class ApiTest {
-    private int weight;
-    private Response response;
-    private JsonPath jsonPath;
+    private static final String BASE_URL = "https://pokeapi.co/api/v2";
 
     @BeforeTest
     public void setup() {
-        RestAssured.baseURI = "https://pokeapi.co/api/v2";
-        weight = given().contentType(ContentType.JSON)
-                .when().get("/pokemon/pidgeotto")
-                .then().extract().path("weight");
+        RestAssured.baseURI = BASE_URL;
     }
 
     @Test
-    public void testWeightTK1() {
-        given().contentType(ContentType.JSON)
-                .when().get("/pokemon/rattata")
-                .then().contentType(ContentType.JSON)
-                .statusCode(200)
-                .body("weight", lessThan(weight));
+    public void testWeightComparison() {
+        Response pidgeottoResponse = given().contentType(ContentType.JSON).when().get("/pokemon/pidgeotto");
+        Response rattataResponse = given().contentType(ContentType.JSON).when().get("/pokemon/rattata");
+        PokemonDTO pidgeotto = pidgeottoResponse.then().statusCode(200).extract().as(PokemonDTO.class);
+        PokemonDTO rattata = rattataResponse.then().statusCode(200).extract().as(PokemonDTO.class);
+        assertTrue(rattata.getWeight() < pidgeotto.getWeight(), "Вес rattata > pidgeotto");
     }
 
+
     @Test
-    public void testRattataAbilityTK2() {
-        String url = "https://pokeapi.co/api/v2/pokemon/rattata";
-        Response response = RestAssured.get(url);
-        response.then().statusCode(200)
-                .body("abilities.ability.name", hasItem("run-away"));
+    public void testRattataAbilitiesTK2() {
+        Response rattataResponse = given().contentType(ContentType.JSON).when().get("/pokemon/rattata");
+        PokemonDTO rattata = rattataResponse.then().statusCode(200).extract().as(PokemonDTO.class);
+        boolean hasGutsAbility = rattata.getAbilities().stream().map(PokemonDTO.AbilityDTO::getAbility).map(PokemonDTO.AbilityDTO.Ability::getName).anyMatch(name -> name.equals("guts"));
+        boolean hasRunAwayAbility = rattata.getAbilities().stream().map(PokemonDTO.AbilityDTO::getAbility).map(PokemonDTO.AbilityDTO.Ability::getName).anyMatch(name -> name.equals("guts"));
+        assertTrue(hasGutsAbility && hasRunAwayAbility, "Не все спопсобности найдены");
     }
+
 
     @Test
     public void testPokemonListTK3() {
-        RestAssured.baseURI = "https://pokeapi.co/api/v2";
-        given().
-                queryParam("limit", 3).
-                when().
-                get("/pokemon").
-                then().
-                assertThat().
-                statusCode(200).
-                body("results.size()", equalTo(3)).
-                body("results.name", everyItem(notNullValue()));
+        given().queryParam("limit", 3).when().get("/pokemon").then().assertThat().statusCode(200).body("results.size()", equalTo(3)).body("results.name", everyItem(notNullValue()));
     }
 }
